0852a93844a924e7c9a88289d704afddb5843b5a
İlke Candan Bengi <64421962+ilkecandan@users.noreply.github.com>
2025-04-17 02:01:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -58,5 +58,5 @@ app.get('/leads', async (req, res) => {
 });
 
 // 🟢 Launch Server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
